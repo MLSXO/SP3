63e6d9efc5a6d8fbfebff2a3ad7c18d83a589c42
MLSXO <marcussperling@me.com>
2025-11-06 21:07:38 +0100

    Update 6/11

--- SP3/src/Series.java
+++ SP3/src/Series.java
@@ -1,2 +1,31 @@
-public class Series {
+import java.util.ArrayList;
+
+public class Series extends Media {
+    private ArrayList<Seasons> seasons;
+
+    public Series(String title, int year, String category, double rating) {
+        super(title, year, category, rating);
+        this.seasons = new ArrayList<>();
+    }
+
+
+    public void addSeason(Seasons season) {
+        seasons.add(season);
+    }
+
+
+    public ArrayList<Seasons> getSeasons() {
+        return seasons;
+    }
+
+
+    public int getNumberOfSeasons() {
+        return seasons.size();
+    }
+
+    @Override
+    public String getInfo() {
+        return "Serie: " + getTitle() + " (" + getYear() + ") - " + getCategory() +
+                " | Rating: " + getRating() + " | Sæsoner: " + getNumberOfSeasons();
+    }
 }
